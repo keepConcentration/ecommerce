5956aa71538f31e0fe87d94e24b85f1eb2e9e9c9
keepConcentration <phm543@gmail.com>
2025-12-03 23:47:30 +0900

    feat: batch용 springboot 모듈 추가

---
+++ batch-server/src/main/java/com/phm/ecommerce/batch_server/BatchServerApplication.java
@@ -0,0 +1,13 @@
+package com.phm.ecommerce.batch_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatchServerApplication.class, args);
+	}
+
+}
---
+++ batch-server/src/test/java/com/phm/ecommerce/batch_server/BatchServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phm.ecommerce.batch_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
