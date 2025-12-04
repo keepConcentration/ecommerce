789eb6814d4fb078986af45238f8b7e1559f1b01
keepConcentration <phm543@gmail.com>
2025-12-04 22:41:30 +0900

    feat: redisTemplate 사용 통일을 위해 cacheManager bean 삭제

--- common/src/main/java/com/phm/ecommerce/config/RedisConfig.java
+++ common/src/main/java/com/phm/ecommerce/config/RedisConfig.java
@@ -6,28 +6,21 @@ import com.fasterxml.jackson.databind.ObjectMapper.DefaultTyping;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.databind.jsontype.impl.StdTypeResolverBuilder;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import java.time.Duration;
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cache.CacheManager;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.cache.RedisCacheConfiguration;
-import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
-import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
-@EnableCaching
 public class RedisConfig {
 
   @Value("${spring.data.redis.host:localhost}")
@@ -83,27 +76,6 @@ public class RedisConfig {
   public GenericJackson2JsonRedisSerializer jsonRedisSerializer(
       @Qualifier("redisObjectMapper") ObjectMapper redisObjectMapper) {
     return new GenericJackson2JsonRedisSerializer(redisObjectMapper);
-  }
-
-  @Bean
-  public CacheManager cacheManager(
-      RedisConnectionFactory connectionFactory,
-      StringRedisSerializer stringRedisSerializer,
-      GenericJackson2JsonRedisSerializer jsonRedisSerializer) {
-    RedisCacheConfiguration defaultConfig = RedisCacheConfiguration.defaultCacheConfig()
-        .entryTtl(Duration.ofMinutes(10))
-        .serializeKeysWith(
-            RedisSerializationContext.SerializationPair.fromSerializer(stringRedisSerializer))
-        .serializeValuesWith(
-            RedisSerializationContext.SerializationPair.fromSerializer(jsonRedisSerializer));
-
-    RedisCacheConfiguration productConfig = defaultConfig
-        .entryTtl(Duration.ofMinutes(30));
-
-    return RedisCacheManager.builder(connectionFactory)
-        .cacheDefaults(defaultConfig)
-        .withCacheConfiguration("product", productConfig)
-        .build();
   }
 
   @Bean
