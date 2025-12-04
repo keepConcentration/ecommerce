44544bcf84b802b1d6ddbd3035f6a35e168f9f21
keepConcentration <phm543@gmail.com>
2025-12-04 13:33:23 +0900

    refactor: batch 서버 테스트 세팅

--- batch-server/src/test/java/com/phm/ecommerce/batch_server/BatchServerApplicationTests.java
+++ batch-server/src/test/java/com/phm/ecommerce/batch_server/BatchServerApplicationTests.java
@@ -1,10 +1,11 @@
 package com.phm.ecommerce.batch_server;
 
+import com.phm.ecommerce.support.TestContainerSupport;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class BatchServerApplicationTests {
+class BatchServerApplicationTests extends TestContainerSupport {
 
 	@Test
 	void contextLoads() {
